8adadb27d9bdbb24f777c8dc2768e8a9062ef152
raihanM95 <mahmud.raihan@yahoo.com>
2022-11-30 10:54:00 +0600

    added files

---
+++ Send.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace WinApp
+{
+    partial class Send
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAuth = new System.Windows.Forms.Button();
+            this.listContact = new System.Windows.Forms.ListView();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSendMessage = new System.Windows.Forms.Button();
+            this.picPhoto = new System.Windows.Forms.PictureBox();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.picPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(71, 38);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(98, 30);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(210, 20);
+            this.txtCode.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Code";
+            // 
+            // btnAuth
+            // 
+            this.btnAuth.Location = new System.Drawing.Point(315, 30);
+            this.btnAuth.Name = "btnAuth";
+            this.btnAuth.Size = new System.Drawing.Size(105, 20);
+            this.btnAuth.TabIndex = 3;
+            this.btnAuth.Text = "Auth";
+            this.btnAuth.UseVisualStyleBackColor = true;
+            this.btnAuth.Click += new System.EventHandler(this.btnAuth_Click);
+            // 
+            // listContact
+            // 
+            this.listContact.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listContact.HideSelection = false;
+            this.listContact.Location = new System.Drawing.Point(12, 56);
+            this.listContact.MultiSelect = false;
+            this.listContact.Name = "listContact";
+            this.listContact.Size = new System.Drawing.Size(408, 213);
+            this.listContact.TabIndex = 4;
+            this.listContact.UseCompatibleStateImageBehavior = false;
+            this.listContact.View = System.Windows.Forms.View.Details;
+            this.listContact.SelectedIndexChanged += new System.EventHandler(this.listContact_SelectedIndexChanged);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(88, 276);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(220, 43);
+            this.txtMessage.TabIndex = 5;
+            // 
+            // btnSendMessage
+            // 
+            this.btnSendMessage.Location = new System.Drawing.Point(315, 276);
+            this.btnSendMessage.Name = "btnSendMessage";
+            this.btnSendMessage.Size = new System.Drawing.Size(105, 43);
+            this.btnSendMessage.TabIndex = 6;
+            this.btnSendMessage.Text = "Send Message";
+            this.btnSendMessage.UseVisualStyleBackColor = true;
+            this.btnSendMessage.Click += new System.EventHandler(this.btnSendMessage_Click);
+            // 
+            // picPhoto
+            // 
+            this.picPhoto.Location = new System.Drawing.Point(12, 276);
+            this.picPhoto.Name = "picPhoto";
+            this.picPhoto.Size = new System.Drawing.Size(71, 43);
+            this.picPhoto.TabIndex = 7;
+            this.picPhoto.TabStop = false;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 184;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Phone";
+            // 
+            // Send
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 330);
+            this.Controls.Add(this.picPhoto);
+            this.Controls.Add(this.btnSendMessage);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.listContact);
+            this.Controls.Add(this.btnAuth);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Send";
+            this.Text = "Send";
+            ((System.ComponentModel.ISupportInitialize)(this.picPhoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAuth;
+        private System.Windows.Forms.ListView listContact;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSendMessage;
+        private System.Windows.Forms.PictureBox picPhoto;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ Send.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TeleSharp.TL;
+using TeleSharp.TL.Contacts;
+using TeleSharp.TL.Upload;
+using TLSharp.Core;
+
+namespace WinApp
+{
+    public partial class Send : Form
+    {
+        int apiId = 7547006; //nhap id của bạn
+        string apiHash = "dc0e5e7f3b17254634ee1fe249d30685";
+        TelegramClient client;
+        string myPhoneNumber = "+8801750675743";
+        string hash;
+        TLContacts tLContact;
+        string userIDSelected;
+
+        public Send()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                File.Delete("session.dat");
+                client = new TelegramClient(apiId, apiHash, dcIpVersion: DataCenterIPVersion.OnlyIPv4);
+                await client.ConnectAsync();
+                hash = await client.SendCodeRequestAsync(myPhoneNumber);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        private async void btnAuth_Click(object sender, EventArgs e)
+        {
+            await client.MakeAuthAsync(myPhoneNumber, hash, txtCode.Text);
+            tLContact = await client.GetContactsAsync();
+
+            foreach (TLUser user in tLContact.Users)
+            {
+                if (!string.IsNullOrEmpty(user.Phone))
+                {
+                    var fullName = $"{user.FirstName} {user.LastName}";
+                    var phone = user.Phone;
+                    string[] row = { fullName, phone, user.Id.ToString() };
+                    var listviewItem = new ListViewItem(row);
+                    listContact.Items.Add(listviewItem);
+                }
+            }
+        }
+
+        public Image ReadImageFromUser(TLUser user)
+        {
+            if (user.Photo == null) return Properties.Resources.no_avatar;
+            var photo = ((TLUserProfilePhoto)user.Photo);
+            var photoLocation = (TLFileLocation)photo.PhotoBig;
+
+            TLFile file = client.GetFile(new TLInputFileLocation()
+            {
+                LocalId = photoLocation.LocalId,
+                Secret = photoLocation.Secret,
+                VolumeId = photoLocation.VolumeId
+            }, 1024 * 256).Result;
+
+            Image image;
+            using (var m = new MemoryStream(file.Bytes))
+            {
+                image = Image.FromStream(m);
+            }
+            return image;
+        }
+
+        private void listContact_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listContact.SelectedItems.Count == 0)
+                return;
+            ListViewItem item = listContact.SelectedItems[0];
+            var phone = item.SubItems[1].Text;
+            userIDSelected = item.SubItems[2].Text;
+
+            var user = tLContact.Users
+               .Where(x => x.GetType() == typeof(TLUser))
+               .Cast<TLUser>()
+               .FirstOrDefault(x => x.Phone == phone);
+
+            //var userPhoto = ReadImageFromUser(user);
+            //picPhoto.Image = userPhoto;
+        }
+
+        private async void btnSendMessage_Click(object sender, EventArgs e)
+        {
+            await client.SendMessageAsync(new TLInputPeerUser() { UserId = Convert.ToInt32(userIDSelected) }, txtMessage.Text);
+        }
+    }
+}
